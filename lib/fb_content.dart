738b8259ef3e471de09eb13848295c2712bf6052
thangvnv <tnguyen@moxielabs.co>
2024-03-06 15:25:55 +0700

    Add price and name fields to FbContentItem class

--- lib/fb_content.dart
+++ lib/fb_content.dart
@@ -1,16 +1,22 @@
 class FbContentItem {
   final String id;
   final int quantity;
+  final double? price;
+  final String? name;
 
   const FbContentItem({
     required this.id,
     required this.quantity,
+    this.price,
+    this.name,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'quantity': quantity,
+      if (price != null) 'price': price,
+      if (name != null) 'name': name,
     };
   }
 
@@ -18,6 +24,8 @@ class FbContentItem {
     return FbContentItem(
       id: map['id'],
       quantity: map['quantity'],
+      price: map['price'],
+      name: map['name'],
     );
   }
 }
